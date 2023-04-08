e6a921428dd0511a9de05217a89a89e8bc441131
Laurenz Werner <werner.la@protonmail.com>
2023-04-08 10:54:37 +0200

    licensing under GPLv3

--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,21 @@
+/*	Copyright (c) 2022, 2023 Laurenz Werner
+	
+	This file is part of Dawn.
+	
+	Dawn is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	Dawn is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with Dawn.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 use crate::*;
 
 #[test]
